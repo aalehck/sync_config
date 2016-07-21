34a5a0101fb6ec452b5b87226758aa9107d22b61
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-07-20 19:12:26 -0500

    clean

--- apps/synconf/lib/synconf/config.ex
+++ apps/synconf/lib/synconf/config.ex
@@ -3,27 +3,19 @@ defmodule Synconf.Config do
 
   ## Client API
 
-  # def start_link do
-  #   Agent.start_link(fn -> %Conf{} end)
-  # end
-
   def start_link(filepath) do
-    # Agent.start_link(fn -> Conf.new(filepath) end)
     GenServer.start_link(__MODULE__, Path.absname(filepath))
   end
 
   def get(conf) do
-    # Agent.get(conf, &(&1))
     GenServer.call(conf, :get)
   end
 
   def update(conf) do
-    # Agent.update(conf, fn -> Conf.update(conf) end)
     GenServer.call(conf, :update)
   end
 
   def patch(conf, diff) do
-    # Agent.update(conf, fn -> Conf.patch(conf, diff) end)
     GenServer.call(conf, {:patch, diff})
   end
 

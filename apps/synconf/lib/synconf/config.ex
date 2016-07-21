f7bcdfb1d7dcf567ce84e9b08882231f72de679d
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-07-20 19:05:53 -0500

    changing config from agent to genserver

--- apps/synconf/lib/synconf/config.ex
+++ apps/synconf/lib/synconf/config.ex
@@ -1,21 +1,49 @@
 defmodule Synconf.Config do
-  def start_link do
-    Agent.start_link(fn -> %Conf{} end)
-  end
+  use GenServer
+
+  ## Client API
+
+  # def start_link do
+  #   Agent.start_link(fn -> %Conf{} end)
+  # end
 
   def start_link(filepath) do
-    Agent.start_link(fn -> Conf.new(filepath) end)
+    # Agent.start_link(fn -> Conf.new(filepath) end)
+    GenServer.start_link(__MODULE__, Path.absname(filepath))
   end
 
   def get(conf) do
-    Agent.get(conf, &(&1))
+    # Agent.get(conf, &(&1))
+    GenServer.call(conf, :get)
   end
 
   def update(conf) do
-    Agent.update(conf, fn -> Conf.update(conf) end)
+    # Agent.update(conf, fn -> Conf.update(conf) end)
+    GenServer.call(conf, :update)
   end
 
   def patch(conf, diff) do
-    Agent.update(conf, fn -> Conf.patch(conf, diff) end)
-  end    
+    # Agent.update(conf, fn -> Conf.patch(conf, diff) end)
+    GenServer.call(conf, {:patch, diff})
+  end
+
+  ## Server Callbacks
+
+  def init(filepath) do
+    {:ok, Conf.new(filepath)}
+  end
+
+  def handle_call(:get, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call(:update, _from, state) do
+    new_state = Conf.update(state)
+    {:reply, new_state, new_state}
+  end
+
+  def handle_call({:patch, diff}, _from, state) do
+    new_state = Conf.patch(state, diff)
+    {:reply, new_state, new_state}
+  end
 end
